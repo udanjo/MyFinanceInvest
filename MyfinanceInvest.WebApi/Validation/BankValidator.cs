5e5d6568a4b20b797a5286202de2f050a44fb366
ueverson diego soares pedroso <udanjo_hyuuga@hotmail.com>
2020-10-09 12:37:11 -0300

    Correção

--- MyfinanceInvest.WebApi/Validation/BankValidator.cs
+++ MyfinanceInvest.WebApi/Validation/BankValidator.cs
@@ -8,7 +8,7 @@ namespace MyfinanceInvest.WebApi.Infra.Validation
 
         public BankValidator()
         {
-            RuleFor(m => m.Code).NotEmpty().WithMessage("Código é campo obrigatório!").MinimumLength(2).MaximumLength(5);
+            RuleFor(m => m.Code).NotEmpty().WithMessage("Código é campo obrigatório!").MinimumLength(2).MaximumLength(15);
             RuleFor(m => m.Description).NotEmpty().WithMessage("Descrição é campo obrigatório!").MinimumLength(1).MaximumLength(250); ;
         }
     }
